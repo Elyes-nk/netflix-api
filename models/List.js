b719f5e529941bae0769b060a8a680bdd73397e0
Elyes-nk <elyesnk@gmail.com>
2022-02-13 11:44:07 +0100

    edits

--- models/List.js
+++ models/List.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const ListSchema = new Schema(
   {
     title: { type: String, required: true, unique: true },
-    type: { type: String },
     genre: [
       { type: Schema.Types.ObjectId, ref: 'Genre' }
     ],
