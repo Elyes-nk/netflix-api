b52f4978c665b12236fdcf740b39a909d931151a
Elyes-nk <elyesnk@gmail.com>
2022-02-15 19:38:35 +0100

    edit

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -4,7 +4,7 @@ const Transactions = require("../models/Transaction");
 exports.getAll = async (req, res) => {
     // if (req.user.isAdmin) {
       try {
-        const transactions = await Transactions.find().populate("User").populate("Subscribtion");
+        const transactions = await Transactions.find().populate("user").populate("subscribtion");
         res.status(200).json(transactions.reverse());
       } catch (err) {
         res.status(500).json(err);
